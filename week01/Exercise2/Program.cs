9f98160678e8822db5fcfa1c37f8f11c37ffcfea
Sir-James2742 <omondijames2742@gmail.com>
2025-07-03 15:36:10 +0300

    creating my first program i c#

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,50 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter the percentage scored " );
+        string grade = Console.ReadLine();
+        int scoredGrade = int.Parse(grade);
+        string letter = "";
+        string sign = "";
+        if (scoredGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (scoredGrade < 90 && scoredGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (scoredGrade < 80 && scoredGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (scoredGrade < 70 && scoredGrade >= 60)
+        {
+            letter = "D";
+        }
+        else if (scoredGrade < 60)
+        {
+            letter = "F";
+        }
+        int rem;
+        rem = scoredGrade % 10;
+        if (rem >= 7)
+        {
+            sign = "+";
+        }
+        else
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your Grade is {sign}{letter}");
+        if (scoredGrade > 70)
+        {
+            Console.WriteLine("You have pased");
+        }
+        else
+        {
+            Console.WriteLine("You can still improve");
+        }
     }
 }
