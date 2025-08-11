3ffbb9d29240f023a798fdd2eecaa388cc64f1d8
Sir-James2742 <omondijames2742@gmail.com>
2025-08-11 16:37:51 +0300

    Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("John", "Algebra");
+        Console.WriteLine(assignment1.GetSummary());
+        MathAssignment mathAssignment = new MathAssignment("Jane", "Calculus", "Section 5.1", "Problems 1-10");
+        Console.WriteLine(mathAssignment.GetHomeWorkList());
+        WritingAssignment writingAssignment = new WritingAssignment("Alice", "History", "The Renaissance");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+       
     }
 }
